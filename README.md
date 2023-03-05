76b588004d187e58f505b5ef99539b23505f7c02
rahulkfernandes <rahulkfernandes@gmail.com>
2023-03-05 19:24:23 +0530

    GPT2 Image captioning Implemented

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Image2Caption Generator
 
 ## Description
-The Image2Caption Generator generates a caption based on an uploaded image using the BLIP model (Bootstrapping Language-Image Pre-training for Unified Vision-Language Understanding and Generation).
+The Image2Caption Generator generates captions based on an uploaded image using the BLIP model (Bootstrapping Language-Image Pre-training for Unified Vision-Language Understanding and Generation) and the GPT2 Image Captioning Model.
 
 ## Installation
 Clone Repository
--- image2caption.py
+++ image2caption.py
@@ -1,25 +1,47 @@
 from PIL import Image
 import streamlit as st
 from transformers import BlipProcessor, BlipForConditionalGeneration
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
 
-MODEL = "Salesforce/blip-image-captioning-large"
-
-def get_captions(raw_img, model, processor):
-    inputs = processor(raw_img, return_tensors="pt")
-    gen_kwargs = {"max_new_tokens": 20}
-    out = model.generate(**inputs, **gen_kwargs)
-    pred = processor.decode(out[0], skip_special_tokens=True)
-    return pred
+MODEL1 = "Salesforce/blip-image-captioning-large"
+MODEL2 = "nlpconnect/vit-gpt2-image-captioning"
 
 @st.cache_resource
-def load_model():
-    processor = BlipProcessor.from_pretrained(MODEL)
-    model = BlipForConditionalGeneration.from_pretrained(MODEL)
+def load_model1():
+    # Loads blip-image-captioning-large model
+    processor = BlipProcessor.from_pretrained(MODEL1)
+    model = BlipForConditionalGeneration.from_pretrained(MODEL1)
     return model, processor
 
+@st.cache_resource
+def load_model2():
+    # Loads vit-gpt2-image-captioning model
+    model2 = VisionEncoderDecoderModel.from_pretrained(MODEL2)
+    feature_extractor = ViTImageProcessor.from_pretrained(MODEL2)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL2)
+    return model2, feature_extractor, tokenizer
+
+def get_captions1(raw_img, model, processor):
+    # Generates caption using BLIP model
+    inputs = processor(raw_img, return_tensors="pt")
+    gen_kwargs = {"max_new_tokens": 20}
+    output = model.generate(**inputs, **gen_kwargs)
+    pred = processor.decode(output[0], skip_special_tokens=True)
+    return pred
+
+def get_captions2(raw_img, model, feature_extractor, tokenizer):
+    # Generates caption using GPT2 model
+    pixel_values = feature_extractor(images=raw_img, return_tensors="pt").pixel_values
+    gen_kwargs = {"max_new_tokens": 20}
+    output = model.generate(pixel_values, **gen_kwargs)
+    pred = tokenizer.batch_decode(output, skip_special_tokens=True)
+    return pred[0]
+   
 if __name__== "__main__":
     with st.spinner('Loading Model'):
-        model, processor=load_model()
+        blip_model, processor = load_model1()
+        gpt2_model, feature_ext, tokenizer = load_model2()
+
 
     st.write("""
          # Image2Caption Generator
@@ -31,6 +53,8 @@ if __name__== "__main__":
         image = Image.open(file).convert('RGB')
         st.image(image, use_column_width=True)
 
-        with st.spinner('Generating Caption'):
-            caption = get_captions(image, model, processor).capitalize()
-        st.success(caption)
+        with st.spinner('Generating Captions'):
+            caption1 = get_captions1(image, blip_model, processor)
+            caption2 = get_captions2(image, gpt2_model, feature_ext, tokenizer)
+        st.success(caption1.capitalize())
+        st.success(caption2.capitalize())
