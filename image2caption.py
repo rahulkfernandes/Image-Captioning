9993f85e1ee9fa0094d5f3de9f32337b3ba5227c
rahulkfernandes <rahulkfernandes@gmail.com>
2023-03-06 12:22:12 +0530

    Paraphraser implemeted

--- image2caption.py
+++ image2caption.py
@@ -1,27 +1,23 @@
+import re
 from PIL import Image
 import streamlit as st
 from transformers import BlipProcessor, BlipForConditionalGeneration
-from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
 MODEL1 = "Salesforce/blip-image-captioning-large"
-MODEL2 = "nlpconnect/vit-gpt2-image-captioning"
+MODEL2 = "tuner007/pegasus_paraphrase"
 
 @st.cache_resource(show_spinner=False)
-def load_model1():
-    # Loads blip-image-captioning-large model
+def load_models():
+    # Loads blip-image-captioning-large model & tuner007/pegasus_paraphrase
     processor = BlipProcessor.from_pretrained(MODEL1)
-    model = BlipForConditionalGeneration.from_pretrained(MODEL1)
-    return model, processor
+    blip_model = BlipForConditionalGeneration.from_pretrained(MODEL1)
 
-@st.cache_resource(show_spinner=False)
-def load_model2():
-    # Loads vit-gpt2-image-captioning model
-    model2 = VisionEncoderDecoderModel.from_pretrained(MODEL2)
-    feature_extractor = ViTImageProcessor.from_pretrained(MODEL2)
-    tokenizer = AutoTokenizer.from_pretrained(MODEL2)
-    return model2, feature_extractor, tokenizer
+    tokenizer = PegasusTokenizer.from_pretrained(MODEL2)
+    para_model = PegasusForConditionalGeneration.from_pretrained(MODEL2)
+    return blip_model, processor, para_model, tokenizer
 
-def get_captions1(raw_img, model, processor):
+def get_captions(raw_img, model, processor):
     # Generates caption using BLIP model
     inputs = processor(raw_img, return_tensors="pt")
     gen_kwargs = {"max_new_tokens": 20}
@@ -29,18 +25,28 @@ def get_captions1(raw_img, model, processor):
     pred = processor.decode(output[0], skip_special_tokens=True)
     return pred
 
-def get_captions2(raw_img, model, feature_extractor, tokenizer):
-    # Generates caption using GPT2 model
-    pixel_values = feature_extractor(images=raw_img, return_tensors="pt").pixel_values
-    gen_kwargs = {"max_new_tokens": 20}
-    output = model.generate(pixel_values, **gen_kwargs)
-    pred = tokenizer.batch_decode(output, skip_special_tokens=True)
-    return pred[0]
+def get_response(input_text, model, tokenizer, num_return_sequences, num_beams):
+    # Generates Paraphrases
+    batch = tokenizer(
+        [input_text],
+        truncation=True,
+        padding='longest',
+        max_length=60,
+        return_tensors="pt"
+    )
+    translated = model.generate(
+        **batch,
+        max_length=60,
+        num_beams=num_beams, 
+        num_return_sequences=num_return_sequences, 
+        temperature=1.5
+    )
+    tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+    return tgt_text
    
 if __name__== "__main__":
-    with st.spinner('Loading Model'):
-        blip_model, processor = load_model1()
-        gpt2_model, feature_ext, tokenizer = load_model2()
+    with st.spinner('Loading'):
+        blip_model, processor, para_model, tokenizer = load_models()
 
     st.write("""
          # Image2Caption Generator
@@ -53,7 +59,17 @@ if __name__== "__main__":
         st.image(image, use_column_width=True)
 
         with st.spinner('Generating Captions'):
-            caption1 = get_captions1(image, blip_model, processor)
-            caption2 = get_captions2(image, gpt2_model, feature_ext, tokenizer)
-        st.success(caption1.capitalize())
-        st.success(caption2.capitalize())
+            caption = get_captions(image, blip_model, processor).capitalize()
+            num_beams = 10
+            num_return_sequences = 4
+            paraphrases = get_response(
+                caption,
+                para_model, 
+                tokenizer, 
+                num_return_sequences,
+                num_beams
+            )
+        paraphrases = str(paraphrases)
+        paraphrases = re.sub(r'[.\[\]\']', '', paraphrases)
+        all_phrases = caption + ', ' + paraphrases
+        st.success(all_phrases)
